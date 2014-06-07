37b3182a44e1a03f1d0ae0a37327cbb2042d8fd3
rocktail <oldie.fiction+github@gmail.com>
2014-06-07 12:13:26 +0200

    added HolidayRequestType class

---
+++ src/RockHolidays.Model/Models/HolidayRequestType.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RockHolidays.Model.Models
+{
+    /// <summary>
+    /// Type of holiday request
+    /// </summary>
+    public class HolidayRequestType
+    {
+        /// <summary>
+        /// Identity of holiday request type
+        /// </summary>
+        public int HolidayRequestTypeId { get; set; }
+
+        /// <summary>
+        /// Name of the holiday request type
+        /// </summary>
+        public string Name { get; set; }
+    }
+}
