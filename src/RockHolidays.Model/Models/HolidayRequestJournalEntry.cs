c193adebc1921e854a039df42924aa4397b23f90
rocktail <oldie.fiction+github@gmail.com>
2014-06-07 14:01:15 +0200

    added HolidayRequestJournalEntry class

---
+++ src/RockHolidays.Model/Models/HolidayRequestJournalEntry.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RockHolidays.Model.Models
+{
+    /// <summary>
+    /// Transaction journal for holiday requests
+    /// </summary>
+    public class HolidayRequestJournalEntry
+    {
+        /// <summary>
+        /// Gets or sets identity of holiday request journal entry
+        /// </summary>
+        public int HolidayRequestJournalId { get; set; }
+
+        /// <summary>
+        /// Gets or sets holiday request this entry is about
+        /// </summary>
+        public int HolidayRequest { get; set; }
+
+        /// <summary>
+        /// Gets or sets identity of holiday request this entry is about
+        /// </summary>
+        public int HolidayRequesIdt { get; set; }
+
+        /// <summary>
+        /// Employee who created transaction
+        /// </summary>
+        public virtual Employee Employee { get; set; }
+
+        /// <summary>
+        /// Identity of employee who created this transaction
+        /// </summary>
+        public int EmployeeId { get; set; }
+
+        /// <summary>
+        /// Description of this transaction 
+        /// </summary>
+        public string OperationDescription { get; set; }
+
+        /// <summary>
+        /// Date of entry
+        /// </summary>
+        public DateTime CreatedDate { get; set; }
+    }
+}
