62b5db64f50837e5cd785e5ec02b20a992ba90fb
rocktail <oldie.fiction+github@gmail.com>
2014-06-07 12:22:30 +0200

    added and implemented HolidayRequest class

---
+++ src/RockHolidays.Model/Models/HolidayRequest.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RockHolidays.Model.Models
+{
+    /// <summary>
+    /// Holiday request class
+    /// </summary>
+    public class HolidayRequest
+    {
+        /// <summary>
+        /// Gets or sets identity of holiday request
+        /// </summary>
+        public int HolidayRequestId { get; set; }
+
+        /// <summary>
+        /// Gets or sets beginning date of requested holidays
+        /// </summary>
+        public DateTime StartDate { get; set; }
+
+        /// <summary>
+        /// Gets or sets end date of requested holidays
+        /// </summary>
+        public DateTime EndDate { get; set; }
+
+        /// <summary>
+        /// Gets or sets total amount of holiday working days
+        /// </summary>
+        public decimal TotalDays { get; set; }
+
+        /// <summary>
+        /// Gets or sets employee who handed the request
+        /// </summary>
+        public Employee Employee { get; set; }
+
+        /// <summary>
+        /// Gets or sets identity of employee who handed the request
+        /// </summary>
+        public int EmployeeId { get; set; }
+
+        /// <summary>
+        /// Gets or sets cover for our employee during holiday period
+        /// </summary>
+        public Employee Cover { get; set; }
+
+        /// <summary>
+        /// Gets or sets identityt of the cover for our employee during holiday period
+        /// </summary>
+        public int? CoverId { get; set; }
+
+        /// <summary>
+        /// Gets or sets notes
+        /// </summary>
+        public string Notes { get; set; }
+
+        /// <summary>
+        /// Gets or sets holiday request number
+        /// </summary>
+        public string HolidayRequestNumber { get; set; }
+    }
+}
