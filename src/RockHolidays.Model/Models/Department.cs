704a50a081eeddc25db688f0ad46e0f50742d7d8
rocktail <oldie.fiction+github@gmail.com>
2014-06-07 12:01:03 +0200

    added Department POCO class

---
+++ src/RockHolidays.Model/Models/Department.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RockHolidays.Model.Models
+{
+    /// <summary>
+    /// Department in organization
+    /// </summary>
+    public class Department
+    {
+        /// <summary>
+        /// Department unique identity
+        /// </summary>
+        public int DepartmentId { get; set; }
+    }
+}
