a04d2b06295b3f01da9215a127fcbd9da0db55d7
rocktail <oldie.fiction+github@gmail.com>
2014-06-07 07:56:19 +0200

    project initialization, first commit

--- README.md
+++ README.md
@@ -1,4 +1,36 @@
-rock_holidays
+Rock Holidays
 =============
 
-Software designed to guide the process of holiday planning in your company.
+RockHolidays is software designed to guide the process of holiday planning in your company. 
+
+It takes care of all the hassle and allows both employees and company to stop worrying when it comes to planning, requesting and accepting holidays.
+
+## Some of the planned features
+
+#### Employee
+	- Add and print holiday request
+	- Check status of your holiday requests
+	- Check your holiday summary
+	- Plan your holidays
+	- Export your plan to spreadsheet, iCal or just print it
+
+#### Prinicipal
+
+	- View your employees holiday requests
+	- Accept or deny employees holiday requests
+	- Select replacement to accept holidays when you are not available
+	- View your department(s) holiday plan
+
+#### HR
+
+	- View approved requests
+	- Manage departments
+	- Manage principals
+	- Manage employees
+	- Manage holiday limits
+	- Manage calendars
+
+  
+#### Licensing
+
+Software is released under the GNU General Public License, version 3 (GPLv3) (http://www.gnu.org/copyleft/gpl.html).
---
+++ src/RockHolidays/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("RockHolidays")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RockHolidays")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("2114c234-ca10-483d-8fae-8e7bef506ce7")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
