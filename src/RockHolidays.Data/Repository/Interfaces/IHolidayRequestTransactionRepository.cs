c6493df8e660eef45251f2d0b274437024910ba8
rocktail <oldie.fiction+github@gmail.com>
2014-06-08 09:19:37 +0200

    added holiday request entry repository and interface

---
+++ src/RockHolidays.Data/Repository/Interfaces/IHolidayRequestTransactionRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RockHolidays.Data.Infrastructure;
+using RockHolidays.Model.Models;
+
+namespace RockHolidays.Data.Repository.Interfaces
+{
+    public interface IHolidayRequestJournalEntryRepository : IRepository<HolidayRequestJournalEntry>
+    {
+    }
+}
