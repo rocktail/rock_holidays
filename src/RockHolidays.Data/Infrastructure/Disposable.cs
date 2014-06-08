f6067f37ad7846bdec9865468fc5330f13a3faba
rocktail <oldie.fiction+github@gmail.com>
2014-06-08 09:34:00 +0200

    added disposable base class

---
+++ src/RockHolidays.Data/Infrastructure/Disposable.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RockHolidays.Data.Infrastructure
+{
+    public class Disposable : IDisposable
+    {
+        private bool _isDisposed;
+
+        ~Disposable()
+        {
+            Dispose(false);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        private void Dispose(bool disposing)
+        {
+            if (!this._isDisposed && disposing)
+            {
+                DisposeCore();
+            }
+
+            this._isDisposed = true;
+        }
+
+        protected virtual void DisposeCore()
+        {
+        }
+    }
+}
