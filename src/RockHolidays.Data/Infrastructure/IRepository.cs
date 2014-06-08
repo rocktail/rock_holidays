67c48fd7dd136ac74c6bc3c333552ba9aab9975a
rocktail <oldie.fiction+github@gmail.com>
2014-06-08 08:03:01 +0200

    added interface for domain repositories

---
+++ src/RockHolidays.Data/Infrastructure/IRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RockHolidays.Data.Infrastructure
+{
+    /// <summary>
+    /// Base class for repositories
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public interface IRepository<T> where T : class
+    {
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+        void Delete(Expression<Func<T, bool>> where);
+        T GetById(long id);
+        T GetById(string id);
+        T Get(Expression<Func<T, bool>> where);
+        IEnumerable<T> GetAll();
+        IEnumerable<T> GetMany(Expression<Func<T, bool>> where);
+    }
+}
